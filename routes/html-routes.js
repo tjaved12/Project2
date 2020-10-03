19f204517a754cd46b5720d96738ac24375784cf
madhatter <matthew.rikard@gmail.com>
2020-10-03 14:11:19 -0400

    handlebars and routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,23 +10,23 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.render("signup", {diet_db: email });
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/login");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../views/login.handlebars"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.sendFile(path.join(__dirname, "../views/members.handlebars"));
   });
 
 };
