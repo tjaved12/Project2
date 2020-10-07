b690107c3e05d77a0fdef04c5467db638bb27c55
TJ <tahmeenaowais@yahoo.com>
2020-10-07 13:59:28 -0400

    fixed members.js

--- public/js/members.js
+++ public/js/members.js
@@ -1,60 +1,108 @@
 $(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
- $.get("/api/user_data").then(function(data) {
-   $(".member-name").text(data.email);
-  });
+	// This file just does a GET request to figure out which user is logged in
+	// and updates the HTML on the page
+	$.get('/api/user_data').then(function(data) {
+		console.log(data);
+		var test = data.id;
+		$('.member-name').text(data.email + '  ID:' + test);
 
+		//This GET request get the data from Day1 table and update the Handlebars
+		$.get('/api/user_diet').then(function(data) {
+			//var id = test
+			var result = null;
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].id == test) {
+					result = data[i];
+					break;
+				}
+			}
 
+			$('.diet-breakfast').text(result.breakfast);
+			$('.diet-lunch').text(result.lunch);
+			$('.diet-dinner').text(result.dinner);
+		});
 
-$.get('/api/user_diet').then(function(data){
-  console.log("diet", data)
-  $(".diet-breakfast").text(data[0].breakfast);
-  $(".diet-lunch").text(data[0].lunch);
-  $(".diet-dinner").text(data[0].dinner)
-});
+		$.get('/api/user_diet2').then(function(data) {
+			var result = null;
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].id == test) {
+					result = data[i];
+					break;
+				}
+			}
 
+			$('.diet-breakfast2').text(result.breakfast);
+			$('.diet-lunch2').text(result.lunch);
+			$('.diet-dinner2').text(result.dinner);
+		});
 
-$.get('/api/user_diet2').then(function(data){
-  console.log("diet", data)
-  $(".diet-breakfast2").text(data[0].breakfast);
-  $(".diet-lunch2").text(data[0].lunch);
-  $(".diet-dinner2").text(data[0].dinner)
-});
+		$.get('/api/user_diet3').then(function(data) {
+			var result = null;
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].id == test) {
+					result = data[i];
+					break;
+				}
+			}
 
-$.get('/api/user_diet3').then(function(data){
-  console.log("diet", data)
-  $(".diet-breakfast3").text(data[0].breakfast);
-  $(".diet-lunch3").text(data[0].lunch);
-  $(".diet-dinner3").text(data[0].dinner)
-});
+			$('.diet-breakfast3').text(result.breakfast);
+			$('.diet-lunch3').text(result.lunch);
+			$('.diet-dinner3').text(result.dinner);
+		});
 
-$.get('/api/user_diet4').then(function(data){
-  console.log("diet", data)
-  $(".diet-breakfast4").text(data[0].breakfast);
-  $(".diet-lunch4").text(data[0].lunch);
-  $(".diet-dinner4").text(data[0].dinner)
-});
+		$.get('/api/user_diet4').then(function(data) {
+			var result = null;
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].id == test) {
+					result = data[i];
+					break;
+				}
+			}
 
-$.get('/api/user_diet5').then(function(data){
-  console.log("diet", data)
-  $(".diet-breakfast5").text(data[0].breakfast);
-  $(".diet-lunch5").text(data[0].lunch);
-  $(".diet-dinner5").text(data[0].dinner)
-});
+			$('.diet-breakfast4').text(result.breakfast);
+			$('.diet-lunch4').text(result.lunch);
+			$('.diet-dinner4').text(result.dinner);
+		});
 
-$.get('/api/user_diet6').then(function(data){
-  console.log("diet", data)
-  $(".diet-breakfast6").text(data[0].breakfast);
-  $(".diet-lunch6").text(data[0].lunch);
-  $(".diet-dinner6").text(data[0].dinner)
-});
+		$.get('/api/user_diet5').then(function(data, id) {
+			var result = null;
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].id == test) {
+					result = data[i];
+					break;
+				}
+			}
 
-$.get('/api/user_diet7').then(function(data){
-  console.log("diet", data)
-  $(".diet-breakfast7").text(data[0].breakfast);
-  $(".diet-lunch7").text(data[0].lunch);
-  $(".diet-dinner7").text(data[0].dinner)
-});
+			$('.diet-breakfast5').text(result.breakfast);
+			$('.diet-lunch5').text(result.lunch);
+			$('.diet-dinner5').text(result.dinner);
+		});
 
+		$.get('/api/user_diet6').then(function(data) {
+			var result = null;
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].id == test) {
+					result = data[i];
+					break;
+				}
+			}
+			$('.diet-breakfast6').text(result.breakfast);
+			$('.diet-lunch6').text(result.lunch);
+			$('.diet-dinner6').text(result.dinner);
+		});
+
+		$.get('/api/user_diet7').then(function(data) {
+			var result = null;
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].id == test) {
+					result = data[i];
+					break;
+				}
+			}
+
+			$('.diet-breakfast7').text(result.breakfast);
+			$('.diet-lunch7').text(result.lunch);
+			$('.diet-dinner7').text(result.dinner);
+		});
+	});
 });
