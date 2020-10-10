62bd4d65a1325611e11b3792d221cedbf672c1e0
ksbrazier <kylebrazier@gmail.com>
2020-10-10 14:18:40 -0400

    commit changes for heroku

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,7 @@
 // Import MySQL connection.
 var connection = require("./config.json");
 
+//test
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
